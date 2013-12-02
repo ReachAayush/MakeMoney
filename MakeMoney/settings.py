cff5925ca4d78db34141f1b0059ec8c1ffaea29b
Aayush Agarwal <reach.aayush@gmail.com>
2013-12-02 17:18:10 -0500

    oijfdas

--- MakeMoney/settings.py
+++ MakeMoney/settings.py
@@ -58,6 +58,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'core/static'),
 )
 
 # List of finder classes that know how to find static files in
