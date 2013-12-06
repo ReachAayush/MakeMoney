03c6c05fdcbe272d9eae3e21704f436b224f4b73
Aayush Agarwal <reach.aayush@gmail.com>
2013-12-05 20:49:55 -0500

    added some settings

--- MakeMoney/settings.py
+++ MakeMoney/settings.py
@@ -102,23 +102,21 @@ LOGGING = {'version': 1,
  'loggers': {'django.request': {'handlers': ['mail_admins'],
                                 'level': 'ERROR',
                                 'propagate': True}}}
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-# # Allow all host headers
-# ALLOWED_HOSTS = ['*']
-
-# # Static asset configuration
-# import os
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
