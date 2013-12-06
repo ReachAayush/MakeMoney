85bd0fcb7bd5b95107bcef9e7e1d3af63b3821f3
Aayush Agarwal <reach.aayush@gmail.com>
2013-12-06 00:39:41 -0500

    changed my email sender address

--- beta/views.py
+++ beta/views.py
@@ -233,7 +233,7 @@ verify your email address and complete the registration of your account:
 
   send_mail(subject="Verify your Hustle Account",
               message= email_body,
-              from_email="aayusha+devnull@andrew.cmu.edu",
+              from_email="WallStreetCoders@myhustle.herokuapp.com",
               recipient_list=[new_user.email])
 
 
